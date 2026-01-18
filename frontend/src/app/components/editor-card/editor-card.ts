ec41e359a688150af68abfacd775b2a08d096dc4
Wassim Ben Nacef <wassim.ben-nacef01@etu.umontpellier.fr>
2026-01-18 13:15:01 +0100

    feat: add editor-card component

---
+++ frontend/src/app/components/editor-card/editor-card.ts
@@ -0,0 +1,41 @@
+import { Component, input, output } from '@angular/core';
+import { Editor } from '../../interfaces/editor';
+
+@Component({
+  selector: 'app-editor-card',
+  imports: [],
+  templateUrl: './editor-card.html',
+  styleUrl: './editor-card.css'
+})
+export class EditorCardComponent {
+  public editor = input<Editor>();
+  editEditor = output<Editor>();
+  deleteEditor = output<number>();
+  viewGames = output<number>();
+
+  onEdit() {
+    const ed = this.editor();
+    if (ed) {
+      this.editEditor.emit(ed);
+    }
+  }
+
+  onDelete() {
+    const ed = this.editor();
+    if (ed) {
+      const confirmDelete = confirm(
+        `Etes-vous sur de vouloir supprimer l'editeur "${ed.name}" ?`
+      );
+      if (confirmDelete) {
+        this.deleteEditor.emit(ed.id);
+      }
+    }
+  }
+
+  onViewGames() {
+    const ed = this.editor();
+    if (ed) {
+      this.viewGames.emit(ed.id);
+    }
+  }
+}
